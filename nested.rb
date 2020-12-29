4d229d6f539f03e9aef5e1656765940f9f36f5a0
Nyasha Perry <nyashaperry2005@yahoo.com>
2020-12-29 03:51:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,9 +88,6 @@ programmer_hash
 end
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-  # return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -107,7 +104,7 @@ def changing_alan
       }
     }
 
-programmer_hash[:]
+programmer_hash[:alan_kay][:known_for] = "GIU"
 end
 
 def adding_to_dennis
